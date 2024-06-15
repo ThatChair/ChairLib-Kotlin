384f58e0c8929c12b373f9dacbb465727c87ab3f
Thatcher Moore <teammoorefamily@gmail.com>
2024-06-14 22:00:12 -0700

    Initial commit

---
+++ src/test/kotlin/math/CubicSplineTest.kt
@@ -0,0 +1,60 @@
+package math
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class CubicSplineTest {
+
+    @Test
+    fun getAngle1() {
+    }
+
+    @Test
+    fun getAngle2() {
+    }
+
+    @Test
+    fun getA() {
+    }
+
+    @Test
+    fun setA() {
+    }
+
+    @Test
+    fun getB() {
+    }
+
+    @Test
+    fun setB() {
+    }
+
+    @Test
+    fun getC() {
+    }
+
+    @Test
+    fun setC() {
+    }
+
+    @Test
+    fun getD() {
+    }
+
+    @Test
+    fun setD() {
+    }
+
+    @Test
+    fun getY() {
+    }
+
+    @Test
+    fun getSlope() {
+    }
+
+    @Test
+    fun testToString() {
+    }
+}
